a39eeb0606bbcd8e46fa051e61bb96ffd0142413
Amos <amos.rotich@strathmore.edu>
2023-08-18 19:43:18 +0300

    added css

--- frontend/src/components/SearchTextList.jsx
+++ frontend/src/components/SearchTextList.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import {Link, useNavigate} from "react-router-dom";
+
+
+
+
+
 
 function SearchTextList({ searchTexts, onSearchTextClick }) {
   return (
+    
     <div>
-      <h2>All Products</h2>
+      <h2 >All Products</h2>
       <ul>
         {searchTexts.map((searchText, index) => (
           <li key={index} onClick={() => onSearchTextClick(searchText)}>
             <button>{searchText}</button>
           </li>
         ))}
-        
+
       </ul>
     </div>
   );
